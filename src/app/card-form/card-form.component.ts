b86beeb5d506ea04f99a14cc35b5ba7ef3b3465e
Tony Wang <Tony94042@gmail.com>
2023-10-11 17:23:09 +1100

    Started validation again

---
+++ src/app/card-form/card-form.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+//FormGroup - Represents a single input field 
+//FormControl - Represents a group of controls. It can also contain nested groups
+
+@Component({
+  selector: 'app-card-form',
+  templateUrl: './card-form.component.html',
+  styleUrls: ['./card-form.component.css']
+})
+export class CardFormComponent {
+  cardForm = new FormGroup ({
+    name: new FormControl('',[Validators.required,Validators.minLength(3)])
+  })
+}
